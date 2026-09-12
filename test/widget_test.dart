b23613807fd1bfd55f33cf493cb078d8fdd979c4
jayajitdutta <jayajitdutta7@gmail.com>
2026-09-12 14:57:39 +0530

    Deployment fixation--||x2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:jayajit_portfolio/main.dart';
 import 'package:jayajit_portfolio/data/profile_data.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  GoogleFonts.config.allowRuntimeFetching = false;
+
   testWidgets('Portfolio home page renders hero name', (tester) async {
     await tester.pumpWidget(
       const ProviderScope(
