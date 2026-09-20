03b59280c9515d49bd7dc66a1d788b0afa2d1ef1
jayajitdutta <jayajitdutta7@gmail.com>
2026-09-20 20:50:02 +0530

    True multiplatform , Sync Android , IOS , Desktop

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "jayajit_portfolio");
+    gtk_header_bar_set_title(header_bar, "Jayajit Dutta - Senior Android & Flutter Developer");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "jayajit_portfolio");
+    gtk_window_set_title(window, "Jayajit Dutta - Senior Android & Flutter Developer");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
@@ -115,6 +115,10 @@ static void my_application_class_init(MyApplicationClass* klass) {
 }
 
 static void my_application_init(MyApplication* self) {}
+
+#ifndef APPLICATION_ID
+#define APPLICATION_ID "com.jayajitdutta.portfolio"
+#endif
 
 MyApplication* my_application_new() {
   // Set the program name to the application ID, which helps various systems
